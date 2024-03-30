12d5319e25b5ab0f8ce19f6e3b24b62d34cd6b9f
vaishnaviguhe <vaishnaviguhe23@navgurukul.org>
2024-03-30 10:37:11 +0530

    quotes

---
+++ Assignment1.js
@@ -0,0 +1,101 @@
+// 1. Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.//
+
+// let a=10; 
+// let b=30; 
+
+// let sum        = a+b;
+// let difference = a-b; 
+// let product    = a*b;
+// let quotient   = a/b;
+// let modulus    = a%b;  
+
+// console.log(`sum:${sum}`); 
+// console.log(`difference:${difference}`);
+// console.log(`product:${product}`);
+// console.log(`quotient:${quotient}`);
+// console.log(`modulus:${modulus}`); 
+
+
+// 2. Take two numbers and only print the integer part when a is divided by b. 
+
+// let a = 25 ;
+// let b =  4 ;
+// console.log(a % b);
+
+
+// 3. Take two numbers a,b and print the exponential power of the first number raised by the second.
+// let a=25;
+// let b=2;
+// console.log(a**b)
+   
+
+
+
+
+// 4. Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+// let a=25;
+// let b=3;
+// console.log(a/b);
+
+// 5. Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+// let a=26;
+// let b=30;
+// let c=29;
+// let d=25
+// let e=23;
+// console.log(a+b+c+d+e/5);
+
+
+// 6. Take the cost price a and selling price b of a product, and print the profit obtained on the product.
+// let cost_price=32;
+// let selling_price=6;
+// console.log(cost_price-selling_price);
+
+
+// 7. Take a number and print out the last digit of the number.
+// let n = 40;
+// console.log(n % 10);
+
+//  8. Take a 2 digit number and return the first and last digits.
+// let number = 23;
+// let firstDigit = Math.floor(number / 10); 
+// let lastDigit = number % 10;
+// console.log(firstDigit);
+// console.log(lastDigit);
+
+ 
+//  9. Write a JavaScript program to find the largest of three numbers.
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// let largest = a; 
+
+// if (b > largest) {
+//     largest = b;
+// }
+
+// if (c > largest) {
+//     largest = c;
+// }
+
+// console.log("The largest number is: " + largest);
+
+
+// 10. Write a JavaScript program to check whether a number is even or odd.
+// let n = 4 ;
+// if (n%2==0){
+//     console.log("even"); 
+// }
+// else{
+//     console.log("odd");
+// } 
+
+//  11. Write a JavaScript program to check whether a given number is prime or not.
+
+//  12. Write a JavaScript program to convert temperature from Celsius to Fahrenheit and vice versa.
+
+// Fahrenheit to Celsius
+// let fahrenheit2 = 212;
+// let celsius2 = (fahrenheit2 - 32) * 5/9;
+// console.log(`Celsius: ${celsius2}`);
